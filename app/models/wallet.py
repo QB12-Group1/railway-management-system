91eea48cb0e9974911d55c082b46cfff50c17a75
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-06-23 02:46:24 +0330

    refactor(models): refactor wallet model

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,4 +1,4 @@
 class Wallet:
-    def __init__(self, balance: float = 0.0, linkedCard=None):
+    def __init__(self, balance: float = 0.0, linked_card: str | None = None) -> None:
         self.balance = balance
-        self.linkedCard = linkedCard
+        self.linkedCard = linked_card
