bd6719ce9351ff654e8a2162c307c2e530c3797b
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-06-23 00:42:31 +0330

    style(app/models): improve type hinting

--- app/models/ticket.py
+++ app/models/ticket.py
@@ -1,5 +1,7 @@
 class Ticket:
-    def __init__(self, destination_station: str, train_name: str, ticket_price: int):
+    def __init__(
+        self, destination_station: str, train_name: str, ticket_price: int
+    ) -> None:
         self.destination_station = destination_station
         self.train_name = train_name
         self.ticket_price = ticket_price
