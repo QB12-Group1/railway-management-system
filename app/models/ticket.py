c495c46e7ca9c53c83f1ba6315300dfbba7066c6
farhadinanloo <farhad.inan@yahoo.com>
2026-06-22 12:52:57 -0800

    implement ticket model

---
+++ app/models/ticket.py
@@ -0,0 +1,5 @@
+class Ticket:
+    def __init__(self, destination_station: str, train_name: str, ticket_price: int):
+        self.destination_station = destination_station
+        self.train_name = train_name
+        self.ticket_price = ticket_price
