9054ffee4ecc84a4c2b4fcbb57af54c563a86197
Bahar <baharmansourbeigi@gmail.com>
2026-06-23 02:58:57 +0330

    Implement Railway and Train Models

---
+++ app/models/railway_model.py
@@ -0,0 +1,6 @@
+class Railway:
+    def __init__(self, name: str, origin: str, destination: str, stations: list):
+        self.name = name
+        self.origin = origin
+        self.destination = destination
+        self.stations = stations
