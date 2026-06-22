d0d290a503dec9528455c092e0b9aa3e9e90112e
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-06-23 02:20:11 +0330

    style(app/models): improve type hinting

--- app/models/railway.py
+++ app/models/railway.py
@@ -1,5 +1,7 @@
 class Railway:
-    def __init__(self, name: str, origin: str, destination: str, stations: list):
+    def __init__(
+        self, name: str, origin: str, destination: str, stations: list[str]
+    ) -> None:
         self.name = name
         self.origin = origin
         self.destination = destination
